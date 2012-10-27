d2189a4aee187a1df56917cc471c0338bff262cd
Mark Capaldi <mc@heritageit.co.uk>
2012-10-27 18:18:25 +0100

    Changed dotfiles.cmd to explicitly create the symobilic links.  I was trying to be too clever by trying to get it to work without listing the files but it proved to be beyond my ability\!

--- dotfiles.cmd
+++ dotfiles.cmd
@@ -1,2 +1,13 @@
-rem echo off
-for %%i in (*) do mklink /h "%HOME%"\%%i %~f1\%%i
+pushd \dotfiles
+
+mklink /h "%HOME%"\.bash_profile .bash_profile
+mklink /h "%HOME%"\.gitconfig .gitconfig
+mklink /h "%HOME%"\.gitignore .gitignore
+mklink /h "%HOME%"\.gitk .gitk
+mklink /h "%HOME%"\.vimrc .vimrc
+mklink /h "%HOME%"\_viemurc _viemurc
+mklink /h "%HOME%"\TestNamingMode.ahk TestNamingMode.ahk
+mklink /h "%HOME%"\testnamingmode_16.ico testnamingmode_16.ico
+mklink /h "%HOME%"\testnamingmode_disabled_16.ico testnamingmode_disabled_16.ico
+
+popd
