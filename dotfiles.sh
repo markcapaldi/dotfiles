5d91f1ced2b37bfaca25c1592e11f375d2b43e89
Mark Capaldi <mc@heritageit.co.uk>
2012-10-27 22:49:52 +0100

    Added dotfiles.sh which configures *nix systems.  However, it must be copied to an executable location like /usr/bin and made executable before it will run.

---
+++ dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s -f ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s -F ~/dotfiles/.vim ~/.vim
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/.gitignore ~/.gitignore
