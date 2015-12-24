db398735b5e5842745190289a6b987bfffc9cabf
Mark Capaldi <mc@heritageit.co.uk>
2015-12-24 21:47:36 +0000

    Made changes to work with recent Windows Server 2012 deployment.

--- dotfiles.cmd
+++ dotfiles.cmd
@@ -1,15 +1,15 @@
 pushd \dotfiles
 
-mklink /h "%HOME%"\.bash_profile .bash_profile
-mklink /h "%HOME%"\.gitconfig .gitconfig
-mklink /h "%HOME%"\.gitignore .gitignore
-mklink /h "%HOME%"\.gitk .gitk
-mklink /h "%HOME%"\.vimrc .vimrc
-mklink /h "%HOME%"\_viemurc _viemurc
-mklink /h "%HOME%"\TestNamingMode.ahk TestNamingMode.ahk
-mklink /h "%HOME%"\testnamingmode_16.ico testnamingmode_16.ico
-mklink /h "%HOME%"\testnamingmode_disabled_16.ico testnamingmode_disabled_16.ico
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\.bash_profile .bash_profile
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\.gitconfig .gitconfig
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\.gitignore .gitignore
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\.gitk .gitk
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\.vimrc .vimrc
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\_viemurc _viemurc
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\TestNamingMode.ahk TestNamingMode.ahk
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\testnamingmode_16.ico testnamingmode_16.ico
+mklink /h "%HOMEDRIVE%%HOMEPATH%"\testnamingmode_disabled_16.ico testnamingmode_disabled_16.ico
 
-mklink /j "%HOME%"\vimfiles .\.vim
+mklink /j "%HOMEDRIVE%%HOMEPATH%"\.vim .\.vim
 
 popd
