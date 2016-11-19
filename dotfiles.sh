278b6ce624cb76976c5bca575c0c6db31a1e8778
Mark Capaldi <mc@heritageit.co.uk>
2016-11-19 18:18:48 +0000

    Updated the shell script after setting up new laptop.

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd ~/dotfiles
-
 ln -s -f ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s -F ~/dotfiles/.vim ~/.vim
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
 
 git submodule init
 git submodule update
